1c026dea483b6fc3a15dc78fa05251e4c1be8ec0
C836 <gl.sepiol@gmail.com>
2022-03-04 15:57:16 -0300

    modo anônimo e melhora na interface de login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = require('http').createServer(resposta);
 var fs = require('fs');
 var io = require('socket.io')(app);
 var usuarios = [];
+var ultimas_mensagens = [];
 
 app.listen(process.env.PORT || 8080);
 console.log("Aplicação está em execução...");
@@ -28,23 +29,54 @@ function resposta (req, res) {
 
 io.on("connection", function(socket){
     socket.on("entrar", function(apelido, callback){
-            if(!(apelido in usuarios)){
-                socket.apelido = apelido;
-                usuarios[apelido] = socket;
+    apelido=apelido.replace(/[`~!@#$%&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
 
-                io.sockets.emit("atualizar usuarios", Object.keys(usuarios));
-                io.sockets.emit("atualizar mensagens", "[ " + pegarDataAtual() + " ] ", apelido , " acabou de entrar na sala");
+        if(!(apelido in usuarios)){
+            socket.apelido = apelido;
+            usuarios[apelido] = socket;
 
-                callback(true);
-            }else{
-                callback(false);
+            for(indice in ultimas_mensagens){
+                socket.emit("atualizar mensagens", `<span style="color:grey">${ultimas_mensagens[indice]}</span>`,'','');
             }
-        });
-        socket.on("enviar mensagem", function(mensagem_enviada, callback){
-            io.sockets.emit("atualizar mensagens", "[ " + pegarDataAtual() + " ] ",socket.apelido, " diz: " + mensagem_enviada);
-            callback();
-        });
+
+            io.sockets.emit("atualizar usuarios", Object.keys(usuarios));
+            io.sockets.emit("atualizar mensagens", "[ " + pegarDataAtual() + " ] ", apelido , " acabou de entrar na sala");
+
+            callback(true);
+        }else{
+            callback(false);
+        }
     });
+    
+    socket.on("enviar mensagem", function(dados, callback){
+
+        var mensagem_enviada = dados.msg;
+        var usuario = dados.usu;
+
+        if(usuario == null)
+            usuario = '';
+
+            var obj_mensagem = "[ " + pegarDataAtual() + " ] " + socket.apelido + " diz: " + mensagem_enviada;
+
+            if(usuario == ''){
+                    socket.emit("atualizar mensagens", "<span style='color:#CDCDCD'>[ " + pegarDataAtual() + " ] ",socket.apelido, " diz: " + mensagem_enviada) + "</span>";
+                    socket.broadcast.emit("atualizar mensagens", "[ " + pegarDataAtual() + " ] ",socket.apelido, " diz: " + mensagem_enviada);
+                    
+                    armazenaMensagem(obj_mensagem);
+            }else{
+                    socket.emit("atualizar mensagens", "<span style='color:#CDCDCD'>[ " + pegarDataAtual() + " ] ","você", ` diz para <span style='color:red'>${usuario}</span>: ` + mensagem_enviada) + "</span>";
+                    usuarios[usuario].emit("atualizar mensagens", "<span style='color:grey'>[ " + pegarDataAtual() + " ] ",socket.apelido, " diz para você: " + mensagem_enviada + "</span>");
+            }
+     
+            callback();
+    });
+
+    socket.on("disconnect", function(){
+        delete usuarios[socket.apelido];
+        io.sockets.emit("atualizar usuarios", Object.keys(usuarios));
+        io.sockets.emit("atualizar mensagens", "[ " + pegarDataAtual() + " ] " ,socket.apelido, " saiu da sala");
+    });
+});
 
 function pegarDataAtual(){
  var dataAtual = new Date();
@@ -56,3 +88,10 @@ function pegarDataAtual(){
  return dataFormatada;
 }
 
+function armazenaMensagem(mensagem){
+    if(ultimas_mensagens.length > 5){
+        ultimas_mensagens.shift();
+    }
+
+    ultimas_mensagens.push(mensagem);
+}
